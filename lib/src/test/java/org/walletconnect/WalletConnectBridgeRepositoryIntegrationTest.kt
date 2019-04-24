e4e4e82e5435f79bb17297daa2e927fa4da708f6
Richard Meissner <msc.meissner@gmail.com>
2019-04-24 11:38:21 +0200

    Use hex encoding without prefix

--- lib/src/test/java/org/walletconnect/WalletConnectBridgeRepositoryIntegrationTest.kt
+++ lib/src/test/java/org/walletconnect/WalletConnectBridgeRepositoryIntegrationTest.kt
@@ -2,6 +2,7 @@ package org.walletconnect
 
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
+import org.junit.Test
 import org.walletconnect.impls.FileWCSessionStore
 import org.walletconnect.impls.MoshiPayloadAdapter
 import org.walletconnect.impls.OkHttpTransport
@@ -22,7 +23,7 @@ class WalletConnectBridgeRepositoryIntegrationTest {
         val sessionDir = File("build/tmp/").apply { mkdirs() }
         val sessionStore = FileWCSessionStore(File(sessionDir, "test_store.json").apply { createNewFile() }, moshi)
         val uri =
-            "wc:31d92639-d7ab-452f-811e-336309ad20b0@1?bridge=https%3A%2F%2Fbridge.walletconnect.org&key=d2f410444aaac3f1410a922b520d5ca575332ae692d1d7b6678f0f5a444aaaa3"
+            "wc:9adf0090-de9d-4511-9142-7e7a58d9024f@1?bridge=https%3A%2F%2Fbridge.walletconnect.org&key=e0f0a33a64dca857356db1011829d4a384f85381aa6c027a50c346a5210d9063"
 
         val config = Session.Config.fromWCUri(uri)
         val session = WCSession(
