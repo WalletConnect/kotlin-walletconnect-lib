b44764e79c5e2aec754fc4bd237592975f722270
Sergio Garcia <sergio@argent.xyz>
2019-10-17 15:35:36 +0100

    Fix "eth_sendTransaction" with no value

--- lib/src/main/kotlin/org/walletconnect/impls/MoshiPayloadAdapter.kt
+++ lib/src/main/kotlin/org/walletconnect/impls/MoshiPayloadAdapter.kt
@@ -130,7 +130,7 @@ class MoshiPayloadAdapter(moshi: Moshi) : Session.PayloadAdapter {
         val nonce = data["nonce"] as? String ?: (data["nonce"] as? Double)?.toLong()?.toString()
         val gasPrice = data["gasPrice"] as? String
         val gasLimit = data["gasLimit"] as? String
-        val value = data["value"] as? String ?: throw IllegalArgumentException("value key missing")
+        val value = data["value"] as? String ?: "0x0"
         val txData = data["data"] as? String ?: throw IllegalArgumentException("data key missing")
         return Session.MethodCall.SendTransaction(getId(), from, to, nonce, gasPrice, gasLimit, value, txData)
     }
