67ea8ceefa65869bde81364418716e91b5b7e5b4
Javonne Martin <javonne.martin@gmail.com>
2021-05-24 10:59:43 +0200

    Various improvements in navigating to the wallet, added sslip.io clear text configuration

--- sample/app/src/main/java/io/walletconnect/example/MainActivity.kt
+++ sample/app/src/main/java/io/walletconnect/example/MainActivity.kt
@@ -22,7 +22,9 @@ class MainActivity : Activity(), Session.Callback {
         when(status) {
             Session.Status.Approved -> sessionApproved()
             Session.Status.Closed -> sessionClosed()
-            Session.Status.Connected,
+            Session.Status.Connected -> {
+                navigateToWallet()
+            }
             Session.Status.Disconnected,
             is Session.Status.Error -> {
                 // Do Stuff
@@ -32,6 +34,13 @@ class MainActivity : Activity(), Session.Callback {
 
     override fun onMethodCall(call: Session.MethodCall) {
     }
+
+    private fun navigateToWallet() {
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(ExampleApplication.config.toWCUri())
+        startActivity(i)
+    }
+
     private fun sessionApproved() {
         uiScope.launch {
             screen_main_status.text = "Connected: ${ExampleApplication.session.approvedAccounts()}"
@@ -61,9 +70,6 @@ class MainActivity : Activity(), Session.Callback {
         screen_main_connect_button.setOnClickListener {
             ExampleApplication.resetSession()
             ExampleApplication.session.addCallback(this)
-            val i = Intent(Intent.ACTION_VIEW)
-            i.data = Uri.parse(ExampleApplication.config.toWCUri())
-            startActivity(i)
         }
         screen_main_disconnect_button.setOnClickListener {
             ExampleApplication.session.kill()
@@ -86,6 +92,7 @@ class MainActivity : Activity(), Session.Callback {
                     ::handleResponse
             )
             this.txRequest = txRequest
+            navigateToWallet()
         }
     }
 
